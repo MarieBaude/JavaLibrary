cf1bc16f53681a2c436088044e14934a4e1a1345
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-11-29 09:37:16 +0100

    add

--- src/main/java/com/simplon/java/App.java
+++ src/main/java/com/simplon/java/App.java
@@ -1,13 +1,32 @@
 package com.simplon.java;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
+
 public class App {
+
+	private static int menuData() {
+		int selection;
+		Scanner sc7 = new Scanner (System.in);
+		System.out.println("Selectionner votre option : ");
+		System.out.println("1) Listes des livres");
+		System.out.println("2) Rechercher un livre");
+		System.out.println("3) Modifier un livre");
+		System.out.println("4) Enregistrer un nouveau livre");
+
+		System.out.println("Votre sélection est : ");
+		selection = sc7.nextInt();
+		return selection;
+	}
 	
 	public static void newBook() {
 		Scanner sc = new Scanner(System.in);
@@ -71,26 +90,58 @@ public class App {
 		sc6.close();	 
 	}
 	
-    public static void search() {
+	public static void search() throws IOException {
+		Scanner scWord = new Scanner(System.in);
+	    System.out.print("Saisir votre recherche : ");
+	    String userText = scWord.nextLine();
+	    userText = userText.toLowerCase();
+	    
+	    try {
+	        FileReader filereader = new FileReader("Books.csv");
+	  
+	        CSVReader csvReader = new CSVReaderBuilder(filereader)
+	                                  .withSkipLines(1)
+	                                  .build();
+	        List<String[]> allData = csvReader.readAll();
+	  
+	        for (String[] row : allData) {
+	            for (String cell : row) {
+	                System.out.print(cell + "\t");
+	            }
+	            System.out.println();
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    
+	    /*if (fileOfBook.getTitle().equals(userText)) {
+	    	System.out.println(fileOfBook);
+	    } else {
+	    	System.out.println("Aucune correspondance");
+	    }*/
+    }
+    
+    public static void updateBook() {
     	
     }
 	
-	public static void main( String[] args ) throws FileNotFoundException {
+	public static void main( String[] args ) throws IOException {
     	
     	int userSelected;
     	
     	do {
-    		userSelected = MenuData();
+    		userSelected = menuData();
     		switch(userSelected) {
     		case 1:
     			System.out.println("Liste des livres : ");
     			seeListOfBook();
     			break;
     		case 2:
-    			System.out.println("Rechercher un livre : ");
+    			search();
     			break;
     		case 3:
     			System.out.println("Modifier un livre : ");
+    			updateBook();
     			break;
     		case 4:
      			newBook();
@@ -102,17 +153,5 @@ public class App {
 
     }
 
-	private static int MenuData() {
-		int selection;
-		Scanner sc7 = new Scanner (System.in);
-		System.out.println("Selectionner votre option : ");
-		System.out.println("1) Listes des livres");
-		System.out.println("2) Rechercher un livre");
-		System.out.println("3) Modifier un livre");
-		System.out.println("4) Enregistrer un nouveau livre");
-
-		System.out.println("Votre sélection est : ");
-		selection = sc7.nextInt();
-		return selection;
-	}
+	
 }
